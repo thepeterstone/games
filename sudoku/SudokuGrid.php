786b7a27d90d19e7cd9cdb04c4a726d2f787920c
Peter Stone <thepeterstone@gmail.com>
2012-02-09 09:53:04 -0800

    whitespace cleanup

--- sudoku/SudokuGrid.php
+++ sudoku/SudokuGrid.php
@@ -1,38 +1,38 @@
 <?php
 
 class SudokuGrid {
-	private $grid = array();
-	private $base = 3;
-	private $size = 9;
-	private $chars = array();
+  private $grid = array();
+  private $base = 3;
+  private $size = 9;
+  private $chars = array();
 
-	public function __construct($gridSize = 9) {
-		$this->base = floor(sqrt($gridSize));
-		if (pow($this->base, 2) != $gridSize) {
-			throw new InvalidArgumentException("gridSize must be a perfect square ($gridSize / {$this->base})");
-		}
-		$this->size = $gridSize;
-		$this->chars = range(1, $this->size);
+  public function __construct($gridSize = 9) {
+    $this->base = floor(sqrt($gridSize));
+    if (pow($this->base, 2) != $gridSize) {
+      throw new InvalidArgumentException("gridSize must be a perfect square ($gridSize / {$this->base})");
+    }
+    $this->size = $gridSize;
+    $this->chars = range(1, $this->size);
 
-		for ($i = 0; $i < $this->size; $i++) {
-			$this->grid[$i] = array();
-			for ($j = 0; $j < $this->size; $j++) {
-				$this->grid[$i][$j] = 0;
-			}
-		}
-		return $this;
-	}
+    for ($i = 0; $i < $this->size; $i++) {
+      $this->grid[$i] = array();
+      for ($j = 0; $j < $this->size; $j++) {
+        $this->grid[$i][$j] = 0;
+      }
+    }
+    return $this;
+  }
 
-	public function populate() {
+  public function populate() {
     $c = 0;
-		$stock = array(FALSE);
-		$grid = array();
+    $stock = array(FALSE);
+    $grid = array();
     $elements = pow($this->size, 2);
-		while ($c < $elements) {
-      for ($i=$c+1; $i < $elements; $i++) { 
+    while ($c < $elements) {
+      for ($i=$c+1; $i < $elements; $i++) {
         $stock[$i] = FALSE;
       }
-			if ($stock[$c] === FALSE) $stock[$c] = $this->possible($c);
+      if ($stock[$c] === FALSE) $stock[$c] = $this->possible($c);
       if (empty($stock[$c])) {
         $this->set($c, 0);
         $c--;
@@ -41,40 +41,40 @@ class SudokuGrid {
       }
       shuffle($stock[$c]);
       $this->set($c,array_pop($stock[$c]));
-      
+
       $c++;
-		}
+    }
     return $this->grid;
-	}
+  }
 
-	public function row($index) {
-		return (array)$this->grid[$index];
-	}
+  public function row($index) {
+    return (array)$this->grid[$index];
+  }
 
-	public function column($index) {
-		$ret = array();
-		for ($i = 0; $i < $this->size; $i++) {
-			$ret[] = $this->grid[$i][$index];
-		}
-		return $ret;
-	}
+  public function column($index) {
+    $ret = array();
+    for ($i = 0; $i < $this->size; $i++) {
+      $ret[] = $this->grid[$i][$index];
+    }
+    return $ret;
+  }
 
-	public function region($i, $j) {
-		list($v, $h) = $this->getBoundingBox($i, $j);
-		$ret = array();
-		for ($a = 0; $a < $this->base; $a++) {
-			for ($b = 0; $b < $this->base; $b++) {
-				$ret[] = $this->grid[$v + $a][$h + $b];
-			}
-		}
-		return $ret;
-	}
+  public function region($i, $j) {
+    list($v, $h) = $this->getBoundingBox($i, $j);
+    $ret = array();
+    for ($a = 0; $a < $this->base; $a++) {
+      for ($b = 0; $b < $this->base; $b++) {
+        $ret[] = $this->grid[$v + $a][$h + $b];
+      }
+    }
+    return $ret;
+  }
 
-	public function getBoundingBox($i, $j) {
-		$i_box = $i - ($i % $this->base);
-		$j_box = $j - ($j % $this->base);
-		return array($i_box, $j_box);
-	}
+  public function getBoundingBox($i, $j) {
+    $i_box = $i - ($i % $this->base);
+    $j_box = $j - ($j % $this->base);
+    return array($i_box, $j_box);
+  }
 
   public function possible($index) {
     $i = floor($index / $this->size);
@@ -84,36 +84,36 @@ class SudokuGrid {
     return $stock;
   }
 
-	public function printGrid() {
-		$string = "";
-		foreach ($this->grid as $i => $line) {
-			if (($i % $this->base) == 0) {
-				$string .= str_repeat("-", 2 * ($this->base + 1) * $this->base + 1) . "\n";
-			}
-			foreach ($line as $j => $digit) {
-				if ($j % $this->base == 0) {
-					$string .= "| ";
-				}
-				$string .= ($digit == 0 ? ' ' : $digit) . " ";
-			}
-			$string .= "|\n";
+  public function printGrid() {
+    $string = "";
+    foreach ($this->grid as $i => $line) {
+      if (($i % $this->base) == 0) {
+        $string .= str_repeat("-", 2 * ($this->base + 1) * $this->base + 1) . "\n";
+      }
+      foreach ($line as $j => $digit) {
+        if ($j % $this->base == 0) {
+          $string .= "| ";
+        }
+        $string .= ($digit == 0 ? ' ' : $digit) . " ";
+      }
+      $string .= "|\n";
 
-		}
-		print $string . str_repeat("-", 2 * ($this->base + 1) * $this->base + 1) . "\n";
-		// don't warn if the buffer is empty
-		@ob_flush();
-		return $this;
-	}
+    }
+    print $string . str_repeat("-", 2 * ($this->base + 1) * $this->base + 1) . "\n";
+    // don't warn if the buffer is empty
+    @ob_flush();
+    return $this;
+  }
 
-	public function serialize() {
-		$string = '';
-		foreach ($this->grid as $line) {
-			foreach($line as $digit) {
-				$string .= $digit;
-			}
-		}
-		return $string;
-	}
+  public function serialize() {
+    $string = '';
+    foreach ($this->grid as $line) {
+      foreach($line as $digit) {
+        $string .= $digit;
+      }
+    }
+    return $string;
+  }
 
   public function deserialize($string) {
     $this->size = sqrt(strlen($string));
@@ -140,7 +140,7 @@ class SudokuGrid {
   }
 
   public function elide($level = .5) {
-    for ($i=0; $i < $this->count(); $i++) { 
+    for ($i=0; $i < $this->count(); $i++) {
       if (rand(0,100) < $level * 100) {
         $this->set($i, 0);
       }
--- sudoku/SudokuSolver.php
+++ sudoku/SudokuSolver.php
@@ -2,41 +2,41 @@
 
 require 'SudokuGrid.php';
 class SudokuSolver {
-	private $grid;
+  private $grid;
 
-	public function __construct($initial) {
-		$this->grid = new SudokuGrid();
-		$this->grid->deserialize($initial);
-	}
+  public function __construct($initial) {
+    $this->grid = new SudokuGrid();
+    $this->grid->deserialize($initial);
+  }
 
-	public function solve()
-	{
-		static $c = 0;
-		if ($c++ > 50) {
-			return FALSE;
-		}
-		$stock = array();
-		for ($i=0; $i < $this->grid->count(); $i++) { 
-			if ((int) $this->grid->get($i) == 0) {
-				$stock[$i] = $this->grid->possible($i);
-			} 
-		}
-		$left = 0;
-		foreach ($stock as $index => $values) {
-			if (count($values) == 1) {
-				$this->grid->set($index, array_pop($values));
-			} else {
-				$left++;
-			}
-		}
-		if ($left > 0) {
-			return $this->solve();
-		}
-		return TRUE;
+  public function solve()
+  {
+    static $c = 0;
+    if ($c++ > 50) {
+      return FALSE;
+    }
+    $stock = array();
+    for ($i=0; $i < $this->grid->count(); $i++) {
+      if ((int) $this->grid->get($i) == 0) {
+        $stock[$i] = $this->grid->possible($i);
+      }
+    }
+    $left = 0;
+    foreach ($stock as $index => $values) {
+      if (count($values) == 1) {
+        $this->grid->set($index, array_pop($values));
+      } else {
+        $left++;
+      }
+    }
+    if ($left > 0) {
+      return $this->solve();
+    }
+    return TRUE;
 
-	}
+  }
 
-	public function printGrid() {
-		$this->grid->printGrid();
-	}
+  public function printGrid() {
+    $this->grid->printGrid();
+  }
 }
