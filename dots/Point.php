5fb72fff3166b894734d9619392135e18d76fa26
Peter Stone <thepeterstone@gmail.com>
2012-02-21 17:04:52 -0800

    add some initial tests for dots

--- dots/Point.php
+++ dots/Point.php
@@ -1,11 +1,22 @@
 <?php
 
 class Point {
-	public function __construct() {
+	public $x, $y;
+
+	public function __construct($x = null, $y = null) {
+		if (!is_int($x)) {
+			throw new InvalidArgumentException("$x is not an integer");
+		}
+		if (!is_int($y)) {
+			throw new InvalidArgumentException("$y is not an integer");
+		}
+		$this->x = $x;
+		$this->y = $y;
 		return $this;
 	}
 
-	public function isAdjacentTo(Point $b) {
-		return false;
+	public function isAdjacentTo(Point $p) {
+		$distance = abs($this->x - $p->x) + abs($this->y - $p->y);
+		return $distance === 1;
 	}
 }
