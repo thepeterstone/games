d3e0801d612315941359f29279ed15c895d34ce3
Peter Stone <thepeterstone@gmail.com>
2012-02-26 20:15:57 -0800

    basic functions to add a stone

--- go/Board.php
+++ go/Board.php
@@ -1,26 +1,67 @@
 <?php
 
 class Board {
+	const WHITE = "•";
+	const BLACK = "O";
+	const BLANK = "+";
+
 	private $values = array();
 
 	public function __construct() {
 		for ($i = 0; $i < 81; $i++) {
-			$this->values[$i] = chr(197);
+			$this->values[$i] = self::BLANK; 
 		}
 		return $this;
 	}
 
 	public function __toString() {
-		$square = sqrt(count($this->values));
+		return $this->serialize();
+	}
+
+	public function serialize() {
+		return join("", $this->values);
+	}
+
+	public function printBoard() {
+		$square = (int) sqrt(count($this->values));
+		// we need a perfect square
+		assert($square * $square === count($this->values));
 		foreach ($this->values as $i => $val) {
-			if (($i) % $square === 0) {
+			if ($i % $square === 0) {
 				print "\n" . str_repeat("  |   ", $square) . "\n";
 			}
 			print "- " . $val . " - ";
 		}
-		print "\n" . str_repeat("  ".chr(179)."   ", $square) . "\n";
+		print "\n" . str_repeat("  |   ", $square) . "\n";
 		print "\n";
+	}
+
+	public function white($a, $b = null) {
+		return $this->_move(self::WHITE, $a, $b);
+	}
+
+	public function black($a, $b = null) {
+		return $this->_move(self::BLACK, $a, $b);
+	}
+
+	private function _move($color, $a, $b) {
+		return is_null($b) ? $this->_byOrdinal($color, $a) : $this->_byCoordinates($color, $a, $b);
+	}
+
+	private function _byOrdinal($color, $position) {
+		$this->values[$position - 1] = $color;
+		return true;
+	}
+
+	private function _byCoordinates($color, $x, $y) {
+		$square = (int) sqrt(count($this->values));
+		$position = (($x - 1) * $square) + $y;
+		return $this->_byOrdinal($color, $position);
 	}
 
 
 }
+
+
+require_once '../Exceptions.php';
+class IllegalPlacementException extends GameRulesException {}
--- go/tests/BoardTest.php
+++ go/tests/BoardTest.php
@@ -1,11 +1,31 @@
 <?php
+error_reporting(E_ALL|E_STRICT);
+ini_set('display_errors', TRUE);
 
 require_once 'PHPUnit/Autoload.php';
 require_once dirname(dirname(__FILE__)) . '/Board.php';
 
 class BoardTest extends PHPUnit_Framework_TestCase {
+	private $board;
+
+	public function setUp() {
+		$this->board = new Board();
+	}
 	public function testEmptyBoard() {
-		$board = new Board();
-		$this->markTestIncomplete();
+		$this->assertEquals(str_repeat("+", 81), $this->board->serialize());
+	}
+
+	public function testAddWhiteByCoordinates() {
+		$this->board->white(3,3);
+		$n = Board::BLANK;
+		$w = Board::WHITE;
+		$this->assertRegexp("/\\$n{20}$w\\$n{60}/", $this->board->serialize());
+	}
+
+	public function testAddBlackByOrdinal() {
+		$this->board->black(32);
+		$n = Board::BLANK;
+		$b = Board::BLACK;
+		$this->assertRegexp("/\\$n{31}$b\\$n{49}/", $this->board->serialize());
 	}
 }
